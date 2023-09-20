ccb0a6dbe8f34fca4d706fa8e2458cee1854df88
sadmansakib172 <salim15-9754@diu.edu.bd>
2023-09-20 19:37:12 +0600

    nested component with routes started

---
+++ src/components/About/About.jsx
@@ -0,0 +1,12 @@
+
+
+const About = () => {
+    return (
+        <div>
+            <h2>About Us</h2>
+            <p>About everything you want to know!</p>
+        </div>
+    );
+};
+
+export default About;
